f234df459b261b55edd2014005101429faab19b2
humdrum <jjrajani@gmail.com>
2017-07-01 23:03:49 -0400

    Updates

---
+++ form-submission-handler.js
@@ -0,0 +1,67 @@
+// 'use strict'
+// function validEmail(email) { // see:
+//   var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+//   return re.test(email);
+// }
+// // get all data in form and return object
+// function getFormData() {
+//   var elements = document.getElementById("gform").elements; // all form elements
+//   var fields = Object.keys(elements).map(function(k) {
+//     if(elements[k].name !== undefined) {
+//       return elements[k].name;
+//     // special case for Edge's html collection
+//     }else if(elements[k].length > 0){
+//       return elements[k].item(0).name;
+//     }
+//   }).filter(function(item, pos, self) {
+//     return self.indexOf(item) == pos && item;
+//   });
+//   var data = {};
+//   fields.forEach(function(k){
+//     data[k] = elements[k].value;
+//     var str = ""; // declare empty string outside of loop to allow
+//                   // it to be appended to for each item in the loop
+//     if(elements[k].type === "checkbox"){ // special case for Edge's html collection
+//       str = str + elements[k].checked + ", "; // take the string and append
+//                                               // the current checked value to
+//                                               // the end of it, along with
+//                                               // a comma and a space
+//       data[k] = str.slice(0, -2); // remove the last comma and space
+//                                   // from the  string to make the output
+//                                   // prettier in the spreadsheet
+//     }else if(elements[k].length){
+//       for(var i = 0; i < elements[k].length; i++){
+//         if(elements[k].item(i).checked){
+//           str = str + elements[k].item(i).value + ", "; // same as above
+//           data[k] = str.slice(0, -2);
+//         }
+//       }
+//     }
+//   });
+//   // console.log(data);
+//   return data;
+// }
+//
+// function handleFormSubmit(event) {  // handles form submit withtout any jquery
+//   event.preventDefault();           // we are submitting via xhr below
+//   var data = getFormData();         // get the values submitted in the form
+//   emailjs.send("gmail","email_template", data)
+//   .then(
+//     function(response) {
+//       // console.log("SUCCESS", response);
+//     },
+//     function(error) {
+//       // console.log("FAILED", error);
+//     }
+//   );
+// }
+// function loaded() {
+//   // console.log('contact form submission handler loaded successfully');
+//   // bind to the submit event of our form
+//   var form = document.getElementById('form');
+//   if (form) {
+//     form.addEventListener("submit", handleFormSubmit, false);
+//   }
+// };
+// emailjs.init("user_2YEmEoQVovBLBaU66Vm7R");
+// document.addEventListener('DOMContentLoaded', loaded, false);
